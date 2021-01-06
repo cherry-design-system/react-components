737307904ea73941239c43e474ddc54eb70e97ef
Luan Gjokaj <luan@riangle.com>
2021-01-06 23:34:59 +0100

    feat: poc header component

---
+++ src/Layout/Heading/makeHeading.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { makeHeadingStyles } from "./makeHeading.styles";
+import { localTheme } from "../../theme";
+
+function makeHeading(h) {
+	return ({ children, size, className, id, theme = localTheme }) => {
+		if (h === 1) {
+			return (
+				<h1
+					css={makeHeadingStyles(theme, size, h)}
+					className={className}
+					id={id}
+				>
+					{children}
+				</h1>
+			);
+		}
+		if (h === 2) {
+			return (
+				<h2
+					css={makeHeadingStyles(theme, size, h)}
+					className={className}
+					id={id}
+				>
+					{children}
+				</h2>
+			);
+		}
+		if (h === 3) {
+			return (
+				<h3
+					css={makeHeadingStyles(theme, size, h)}
+					className={className}
+					id={id}
+				>
+					{children}
+				</h3>
+			);
+		}
+		if (h === 4) {
+			return (
+				<h4
+					css={makeHeadingStyles(theme, size, h)}
+					className={className}
+					id={id}
+				>
+					{children}
+				</h4>
+			);
+		}
+		if (h === 5) {
+			return (
+				<h5
+					css={makeHeadingStyles(theme, size, h)}
+					className={className}
+					id={id}
+				>
+					{children}
+				</h5>
+			);
+		}
+		if (h === 6) {
+			return (
+				<h6
+					css={makeHeadingStyles(theme, size, h)}
+					className={className}
+					id={id}
+				>
+					{children}
+				</h6>
+			);
+		}
+	};
+}
+
+export { makeHeading };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export * from "./Layout/Container/Container";
 export * from "./Layout/Grid/Row/Row";
 export * from "./Layout/Grid/Col/Col";
+
+export { H1, H2, H3, H4, H5, H6 } from "./Layout/Heading";
